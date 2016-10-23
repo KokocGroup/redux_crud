02c650e3f5dead5d6eea47b2f1be084796938eef
vchilikov <vchilikov@gmail.com>
2016-10-23 21:35:56 +0300

    add files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var path = require('path');
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 var plugins = [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
     function () {
         this.plugin('watch-run', function (watching, callback) {
             console.log('Begin compile at ' + new Date());
@@ -13,7 +14,7 @@ var plugins = [
 ];
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
     entry: [
         'babel-polyfill',
         './src/index'
@@ -26,15 +27,31 @@ module.exports = {
     },
     plugins: plugins,
     module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+            }
+        ],
         loaders: [
             {
                 loaders: ['babel-loader'],
                 include: [
-                    path.resolve(__dirname, "src")
+                    path.resolve(__dirname, "src"),
                 ],
                 test: /\.js$/,
-                plugins: ['transform-runtime']
+                plugins: ['transform-runtime'],
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
             }
         ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
     }
 };
